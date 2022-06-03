77705dcaefeeedb52a5d7cbbe1e570620c515ef3
KilianCr <kilian.crepin@outlook.fr>
2022-06-03 07:03:51 +0200

    Add Analyse and clean folder

---
+++ PcCleaner/Cleaner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcCleaner
+{
+    internal class Cleaner
+    {
+        private DirectoryInfo winTemp { get; }
+        private DirectoryInfo appTemp{ get; }
+
+    public Cleaner()
+        {
+            winTemp = new DirectoryInfo(@"C:\Windows\Temp");
+            appTemp = new DirectoryInfo(System.IO.Path.GetTempPath());
+        }
+
+        public long DirectorySize(DirectoryInfo dir)
+        {
+            return dir.GetFiles().Sum(f => f.Length) + dir.GetDirectories().Sum(di => DirectorySize(di));
+        }
+        public List<string> Clear(DirectoryInfo dir)
+        {
+            List<string> list = new List<string>();
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                    list.Add(file.FullName);
+
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+
+            }
+            list.Add("-");
+            foreach (DirectoryInfo di in dir.GetDirectories())
+            {
+                try
+                {
+                    di.Delete();
+                    list.Add(di.FullName);
+
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+
+            }
+            return list;
+        }
+    }
+  
+}
--- PcCleaner/MainWindow.xaml.cs
+++ PcCleaner/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,44 @@ namespace PcCleaner
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void clean_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void historic_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void update_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO","Mise à jour",MessageBoxButton.OK,MessageBoxImage.Information);
+        }
+
+        private void website_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("https://www.youtube.com/watch?v=XqZsoesa55w")
+                {
+                    UseShellExecute = true
+                });
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Impossible d'ouvrir le navigateur par defaut\n - "+ ex.Message,"Erreur",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+               
+            
+        }
+        private void analyse_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
